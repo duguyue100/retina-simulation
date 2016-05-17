cd097c7c1a8730a73da01036aa93e09438a107c0
Yuhuang Hu <duguyue100@gmail.com>
2016-05-17 23:01:47 +0200

    update modules

--- simretina/gui.py
+++ simretina/gui.py
@@ -8,6 +8,38 @@ import cv2
 import numpy as np
 
 
+def bgr2rgb(frame):
+    """Convert color span from BGR to RGB.
+
+    Parameters
+    ----------
+    frame : numpy.ndarray
+        a given frame
+
+    Returns
+    -------
+    new_frame : numpy.ndarray
+        a converted frame
+    """
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+
+def trans_bgr2rgb_seq(frames):
+    """Transfer BGR sequence to RGB sequence.
+
+    Parameters
+    ----------
+    frames : list
+        a given BGR sequence
+
+    Returns
+    -------
+    new_frames : list
+        a converted RGB sequence
+    """
+    return [bgr2rgb(frame) for frame in frames]
+
+
 def create_viewer_field(frames, inter_padding=20, border_padding=40,
                         color=[0, 0, 0]):
     """Create Viewer field by gluing multiple frames together.
--- simretina/retina.py
+++ simretina/retina.py
@@ -23,6 +23,11 @@ def init_retina(size):
     return bioinspired.createRetina((size[1], size[0]))
 
 
+def clear_buffers(retina):
+    """Open the eyes after long peroid."""
+    retina.clearBuffers()
+
+
 def get_opl_frame(retina, frame, get_parvo=True, get_magno=True,
                   color_mode="color"):
     """Get Parvo frame by given retina model and original image.
@@ -33,6 +38,10 @@ def get_opl_frame(retina, frame, get_parvo=True, get_magno=True,
         the retina model
     frame : numpy.ndarray
         a frame
+    get_parvo : bool
+        return parvo frames if True, otherwise False
+    get_magno : bool
+        return magno frames if True, otherwise False
     color_mode : string
         indicate color mode, the options are "color", "grey"
 
@@ -78,3 +87,53 @@ def grey2color(frame):
         raise ValueError("Input frame is not a grey frame.")
 
     return np.transpose(np.tile(frame, (3, 1, 1)), (1, 2, 0))
+
+
+def get_opl_frames(retina, frames, get_parvo=True, get_magno=True,
+                   reopen_eye=True, color_mode="color"):
+    """Get provo and magno frames from a sequence of frames.
+
+    Parameters
+    ----------
+    retina : cv2.bioinspired_Retina
+        the retina model
+    frame : list
+        a list of given frames
+    get_parvo : bool
+        return parvo frames if True, otherwise False
+    get_magno : bool
+        return magno frames if True, otherwise False
+    reopen_eye : bool
+        clear buffers if True, else False
+    color_mode : string
+        indicate color mode, the options are "color", "grey"
+    """
+    if len(frames) == 0 or type(frames) != list:
+        raise ValueError("No video frame is entered")
+
+    if reopen_eye is True:
+        clear_buffers(retina)
+
+    if get_parvo is True:
+        parvo_frames = []
+    if get_magno is True:
+        magno_frames = []
+
+    for frame in frames:
+        out_frames = get_opl_frame(retina, frame, get_parvo=get_parvo,
+                                   get_magno=get_magno, color_mode=color_mode)
+
+        if get_parvo is True and get_magno is True:
+            parvo_frames.append(out_frames[0])
+            magno_frames.append(out_frames[1])
+        elif get_parvo is True and get_magno is False:
+            parvo_frames.append(out_frames)
+        elif get_parvo is False and get_magno is True:
+            magno_frames.append(out_frames)
+
+    if get_parvo is True and get_magno is True:
+        return parvo_frames, magno_frames
+    elif get_parvo is True and get_magno is False:
+        return parvo_frames
+    elif get_parvo is False and get_magno is True:
+        return magno_frames
