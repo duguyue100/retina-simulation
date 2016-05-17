741e270296e6c8947075f3aad92160687fea323e
Yuhuang Hu <duguyue100@gmail.com>
2016-05-17 18:50:38 +0200

    add about script

---
+++ simretina/__about__.py
@@ -0,0 +1,14 @@
+"""Meta data for simretina.
+
+Author: Yuhuang Hu
+Email : yuhuang.hu@uzh.ch
+"""
+
+from __future__ import division, print_function, unicode_literals
+
+__all__ = ("__version__", "__author__", "__author_email__", "__url__")
+
+__version__ = "0.1.0"
+__author__ = "Yuhuang Hu, Sonali Andani"
+__author_email__ = "yuhuang.hu@uzh.ch, andanis@student.ethz.ch"
+__url__ = "https://github.com/duguyue100/retina-simulation"
