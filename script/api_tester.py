8e235bce821d1f36f0a3263ed1dc48907193d0fa
Yuhuang Hu <duguyue100@gmail.com>
2016-05-19 22:38:42 +0200

    update api tester

--- script/api_tester.py
+++ script/api_tester.py
@@ -7,9 +7,9 @@ Email : yuhuang.hu@uzh.ch
 import cv2
 from cv2 import bioinspired
 
-from simretina import dataset
+from simretina import dataset, gui
 
-option = "test-retina-class"
+option = "test-bgr2rgb-sequence"
 
 if option == "test-builtin-image":
     # testing for builtin dataset
@@ -27,3 +27,14 @@ if option == "test-retina-class":
     frame, size = dataset.get_lenna()
     retina = bioinspired.createRetina(size[:2])
     print type(retina)
+
+if option == "test-bgr2rgb-sequence":
+    frames, size = dataset.get_horse_riding()
+
+    new_frames = gui.trans_bgr2rgb_seq(frames)
+
+    for frame in new_frames:
+        cv2.imshow("test", frame)
+        cv2.waitKey(delay=0)
+
+    print len(new_frames)
