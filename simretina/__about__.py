5bc8d4d9d68c1c69630d5b4cf4ed4c1eca00d786
Yuhuang Hu <duguyue100@gmail.com>
2016-05-31 19:56:52 +0200

    update version info

--- simretina/__about__.py
+++ simretina/__about__.py
@@ -9,9 +9,9 @@ from __future__ import division, print_function, unicode_literals
 __all__ = ("__version__", "__author__", "__author_email__", "__url__",
            "__download_url__")
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Yuhuang Hu, Sonali Andani"
 __author_email__ = "yuhuang.hu@uzh.ch, andanis@student.ethz.ch"
 __url__ = "https://github.com/duguyue100/retina-simulation"
 __download_url__ = "https://github.com/duguyue100/" + \
-                   "retina-simulation/tarball/0.1.1"
+                   "retina-simulation/tarball/0.1.2"
